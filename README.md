57d98a77f42eddaece9d11f845eb66cc535e533f
Madhu Shesharam <madhu.shesharam@gmail.com>
2019-01-20 00:34:12 -0600

    Authentication JWT

--- README.md
+++ README.md
@@ -19,14 +19,15 @@ TODO:
  2) python app.py
  
  Example Usage:
- ** POST CALL ** to post ANY JSON messages to KAFKA Producer
- curl -X POST http://localhost:5000/posttoKafka -d ' {
-        "xx44Checkkey1": "val1",
-        "xx2llCheckkey2": "val2",
-        "xx3llCheckkey3": "val3"
- }'
+ 
+ Auth
+ Retreive Token 
+ curl -X POST http://127.0.0.1:5000/auth -d ' {"username" : "user1","password" : "abcxyz"}'
+
+
+ curl -X POST  http://127.0.0.1:5000/messages -H 'authorization: JWT $TOKEN  -d '{"key1": "val1","Key2": "val2"}'
 
 ** GET CALL ** To Read latest single event from Kafka 
 
-
-curl -X GET http://127.0.0.1:5000/getLatestKafka 
+curl -X GET http://localhost:5000/messages -H 'authorization: JWT $TOKEN 
+  
--- app.py
+++ app.py
@@ -1,23 +1,35 @@
 import logging, time,json,datetime
 from flask import Flask,request
 from flask_restful import Resource,Api
+from flask_jwt import JWT, jwt_required, current_identity
+
 from pythonKafkaexampApp import Producer,Consumer
-
-
+from auth import authenticate, identity
 
 
 app = Flask(__name__)
+app.secret_key = 'test'
+# Authentication , JWT will pass the params  username & Password to the function authenticate(username,password)
+# This will create /auth
+jwt = JWT(app, authenticate, identity)
+
+
 api = Api(app)
 
+class homepage(Resource):
+    def get(self):
+        return  'This is Message Simulator for KAFKA ..Welcome to home page ...'
+ 
 class communicate_kafka(Resource):
+    @jwt_required()
     def post(self):
-        data = request.json
+        data = request.get_json()
         a=Producer(data)
         a.run()
         time.sleep(3)
-        #logger.info("POSTING DATA is ")
         return data, 201
     
+    @jwt_required()
     def get(self):
         b=Consumer()
         myvalue = b.run()
@@ -26,7 +38,11 @@ class communicate_kafka(Resource):
         print myvalue
         myvalue = json.loads(myvalue)
         return myvalue , 200
+    
+   
 
+api.add_resource(homepage,'/')
 api.add_resource(communicate_kafka,'/messages')
 
-app.run()    
+
+app.run(port =5000, debug=True)    
